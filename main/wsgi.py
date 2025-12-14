706f027e19f4949e6fa8358714cc48207a3a582c
smplay009 <saghar.safari97@gmail.com>
2025-12-14 12:47:25 +0330

    Commit all project changes for deployment

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for main project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+sys.path.insert(0, os.path.dirname(__file__))
 
-application = get_wsgi_application()
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It works!\n'
+    version = 'Python v' + sys.version.split()[0] + '\n'
+    response = '\n'.join([message, version])
+    return [response.encode()]
