2e73488ea42b59038edb590b5c610474e3e0d030
cookie <d.p0n02i@gmail.com>
2025-12-19 20:53:30 +0300

    Update settings for production and CORS

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,16 @@
 import os
 import sys
+import traceback
 
-sys.path.insert(0, os.path.dirname(__file__))
+# مسیر پروژه‌ی Django
+sys.path.insert(0, '/home/remedybyir/repositories/ndf')
 
-os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    'main.settings'
-)
+# مشخص کردن فایل تنظیمات Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+try:
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+except Exception:
+    with open("/home/remedybyir/repositories/ndf/logs/wsgi_error.log", "w") as f:
+        f.write(traceback.format_exc())
