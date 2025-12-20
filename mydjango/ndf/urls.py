e0a07d20ceef6675739acfb361ee2163ddc3f379
smplay009 <saghar.safari97@gmail.com>
2025-12-20 13:12:46 +0330

    Update URLs with home view on real project

--- mydjango/ndf/urls.py
+++ mydjango/ndf/urls.py
@@ -1,6 +1,15 @@
+from django.contrib import admin  # اینو اضافه کن
 from django.urls import path
+from django.http import HttpResponse
 from .views import RatingAPIView
 
+# view ساده برای مسیر اصلی اپ
+def home(request):
+    return HttpResponse("NDf app is running!")
+
 urlpatterns = [
-    path("api/rating/", RatingAPIView.as_view(), name="rating"),
+    path('admin/', admin.site.urls),       # ادمین اینجا درست میشه
+    path("", home, name="home"),           # مسیر root
+    path("api/rating/", RatingAPIView.as_view(), name="rating"),  # مسیر API
 ]
+
