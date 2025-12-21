35af4cb12b95d6c77b42115d29060e887f8ac41b
cookie <d.p0n02i@gmail.com>
2025-12-21 14:14:52 +0300

    Update rating model, admin display and API logic

--- ndf/urls.py
+++ ndf/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.http import HttpResponse
 from .views import RatingAPIView
 
+# view ساده برای مسیر اصلی اپ
+def home(request):
+    return HttpResponse("NDf app is running!")
+
 urlpatterns = [
-    path("api/rating/", RatingAPIView.as_view(), name="rating"),
+    path("", home, name="home"),  # مسیر root برای /ndf/
+    path("api/rating/", RatingAPIView.as_view(), name="rating"),  # مسیر API
 ]
