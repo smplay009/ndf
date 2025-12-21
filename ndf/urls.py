5a8fc8d7aee09ac614fa311ad4ac3f8d95e52605
cookie <d.p0n02i@gmail.com>
2025-12-21 17:18:44 +0300

    Update url,admin

--- ndf/urls.py
+++ ndf/urls.py
@@ -7,8 +7,7 @@ from .views import RatingAPIView
 def home(request):
     return HttpResponse("NDf app is running!")
 
-urlpatterns = [
-    path('admin/', admin.site.urls),       # ادمین اینجا درست میشه
+urlpatterns = [    
     path("", home, name="home"),           # مسیر root
     path("api/rating/", RatingAPIView.as_view(), name="rating"),  # مسیر API
 ]
