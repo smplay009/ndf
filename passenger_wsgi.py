6e57b38ccdd9d955bc4f2020fd26acc9e45038c7
smplay009 <saghar.safari97@gmail.com>
2025-12-23 13:35:27 +0330

    Update from cPanel changes

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,17 +1,9 @@
-import importlib.machinery
-import importlib.util
 import os
 import sys
 
-
 sys.path.insert(0, os.path.dirname(__file__))
 
-def load_source(modname, filename):
-    loader = importlib.machinery.SourceFileLoader(modname, filename)
-    spec = importlib.util.spec_from_file_location(modname, filename, loader=loader)
-    module = importlib.util.module_from_spec(spec)
-    loader.exec_module(module)
-    return module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
-wsgi = load_source('wsgi', 'app.py')
-application = wsgi.application
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
